5880f4873164deea425ce5892b7f90b775363cd3
lucas alejandro pardieux <lucaspardieux@gmail.com>
2022-07-13 01:07:52 -0300

    front v1.1

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -10,16 +10,16 @@ import arrowright from "../../images/arrow_right.png"
 export default class Landing extends Component {
   render() {
     return (
-    <div className={`${style.page}`}>
-       <img className={`${style.background}`} src={background} alt="Welcome" />
-       <img className={`${style.arrowLeft}`} src={arrowLeft} alt="arrowLeft"></img>
-       <img className={`${style.arrowRight}`} src={arrowright} alt="arrowLeft"></img>
-       <Link className={`${style.link}`} to='/home'><img className={`${style.pandora}`} src={pandora} alt="pandora box" /></Link>
-      
-      <h1>Welcome to the Gamer Cave</h1>
-      <h3>Enter to the cave</h3>
-      <a className={`${style.buton}`} href="/about"><span>ABOUT</span><i></i></a>
-  </div>
+      <div className={`${style.page}`}>
+        <img className={`${style.background}`} src={background} alt="Welcome" />
+        <img className={`${style.arrowLeft}`} src={arrowLeft} alt="arrowLeft"></img>
+        <img className={`${style.arrowRight}`} src={arrowright} alt="arrowLeft"></img>
+        <Link className={`${style.link}`} to='/home'><img className={`${style.pandora}`} src={pandora} alt="pandora box" /></Link>
+          <h1>Welcome to the Gamer Cave</h1>
+          <h3>Enter to the cave</h3>
+        <a className={`${style.butonHome}`} href="/home"><span>HOME</span><i></i></a>
+        <a className={`${style.buton}`} href="/about"><span>ABOUT</span><i></i></a>
+      </div>
     )
   }
 }
